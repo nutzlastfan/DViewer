0c579a16a12dab838aaf97d16489798a3ff70757
Denny Spiegelberg <denny@altglienicker.de>
2025-08-16 13:45:35 +0200

    command.cs added

---
+++ DViewer/Commands.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DViewer
+{
+    // Async ICommand (ohne Parameter)
+    public sealed class DelegateCommand : ICommand
+    {
+        private readonly Func<Task> _executeAsync;
+        private readonly Func<bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public DelegateCommand(Func<Task> executeAsync, Func<bool>? canExecute = null)
+        {
+            _executeAsync = executeAsync ?? throw new ArgumentNullException(nameof(executeAsync));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
+
+        public async void Execute(object? parameter) => await _executeAsync().ConfigureAwait(false);
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    // ICommand mit Parameter
+    public sealed class ParameterCommand : ICommand
+    {
+        private readonly Action<object?> _execute;
+        private readonly Func<object?, bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged;
+
+        public ParameterCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public void Execute(object? parameter) => _execute(parameter);
+
+        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+}
--- DViewer/MainPage.xaml.cs
+++ DViewer/MainPage.xaml.cs
@@ -3,43 +3,7 @@
 
 namespace DViewer
 {
-    // Simplified async ICommand
-    public class DelegateCommand : ICommand
-    {
-        private readonly Func<System.Threading.Tasks.Task> _executeAsync;
-        private readonly Func<bool>? _canExecute;
 
-        public event EventHandler? CanExecuteChanged;
-
-        public DelegateCommand(Func<System.Threading.Tasks.Task> executeAsync, Func<bool>? canExecute = null)
-        {
-            _executeAsync = executeAsync;
-            _canExecute = canExecute;
-        }
-
-        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
-        public async void Execute(object? parameter) => await _executeAsync();
-        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-    }
-
-    // Parameterized ICommand
-    public class ParameterCommand : ICommand
-    {
-        private readonly Action<object?> _execute;
-        private readonly Func<object?, bool>? _canExecute;
-
-        public event EventHandler? CanExecuteChanged;
-
-        public ParameterCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
-        {
-            _execute = execute;
-            _canExecute = canExecute;
-        }
-
-        public bool CanExecute(object? parameter) => _canExecute?.Invoke(parameter) ?? true;
-        public void Execute(object? parameter) => _execute(parameter);
-        public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
-    }
 
 
     public class MetadataDifference
